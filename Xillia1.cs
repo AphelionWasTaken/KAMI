b03e77290556804765fa47b0fe9a6c52df7f26aa
Aphelion <61633628+AphelionWasTaken@users.noreply.github.com>
2021-04-29 15:52:02 -0500

    updated Xillia1.cs to include deref chain

--- Xillia1.cs
+++ Xillia1.cs
@@ -1,23 +1,37 @@
-﻿using KAMI.Cameras;
 using System;
+using KAMI.Cameras;
+using KAMI.Utilities;
 
 namespace KAMI.Games
 {
-    public class Xillia1 : Game<HAVACamera>
+    class Xillia1 : Game<HAVACamera>
     {
-        const uint m_address = 0x00E3E64C;
+
+        const uint BaseAddress = 0xE1552C;
+
+        DerefChain m_hor;
+        DerefChain m_vert;
 
         public Xillia1(IntPtr ipc) : base(ipc)
         {
+            var baseChain = DerefChain.CreateDerefChain(ipc, BaseAddress);
+            m_hor = baseChain.Chain(0x78);
+            m_vert = baseChain.Chain(0x7C);
         }
 
         public override void UpdateCamera(int diffX, int diffY)
         {
-            m_camera.Vert = IPCUtils.ReadFloat(m_ipc, m_address + 4);
-            m_camera.Hor = IPCUtils.ReadFloat(m_ipc, m_address);
+            if (DerefChain.VerifyChains(m_hor, m_vert))
+            {
+            m_camera.Hor = (float)(IPCUtils.ReadFloat(m_ipc, (uint)m_hor.Value));
+            m_camera.Vert = (float)(IPCUtils.ReadFloat(m_ipc, (uint)m_vert.Value));
+            
             m_camera.Update(diffX * SensModifier, diffY * SensModifier);
-            IPCUtils.WriteFloat(m_ipc, m_address + 4, m_camera.Vert);
-            IPCUtils.WriteFloat(m_ipc, m_address, m_camera.Hor);
+
+            IPCUtils.WriteFloat(m_ipc, (uint)m_hor.Value, m_camera.Hor);
+            IPCUtils.WriteFloat(m_ipc, (uint)m_vert.Value, m_camera.Vert);
+            
+            }
         }
     }
 }
